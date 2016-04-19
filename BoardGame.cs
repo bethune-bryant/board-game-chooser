118ffb31880ecf6cb5b7afa3ce4df5c83ab22458
bethune-bryant <bryantstutoringservice@gmail.com>
2016-04-19 15:10:08 -0500

    Adding initial code base.

---
+++ BoardGame.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoardGameChooser
+{
+    class BoardGame
+    {
+        public string Name { get; set; }
+        public int MinNumOfPlayers { get; set; }
+        public int MaxNumOfPlayers { get; set; }
+        public int MinDuration { get; set; }
+        public int MaxDuration { get; set; }
+        public List<GameType> Types { get; set; }
+
+        public override string ToString()
+        {
+            return Name;
+        }
+
+        public BoardGame(string Name, int MinNumOfPlayers, int MaxNumOfPlayers, int MinDuration, int MaxDuration, List<GameType> Types)
+        {
+            this.Name = Name;
+            this.MinNumOfPlayers = MinNumOfPlayers;
+            this.MaxNumOfPlayers = MaxNumOfPlayers;
+            this.MinDuration = MinDuration;
+            this.MaxDuration = MaxDuration;
+            this.Types = Types;
+        }
+
+        public BoardGame(string Name, int MinNumOfPlayers, int MaxNumOfPlayers, int MinDuration, int MaxDuration) : this(Name, MinNumOfPlayers, MaxNumOfPlayers, MinDuration, MaxDuration, new List<GameType>()) { }
+
+        public BoardGame(string Name, int MinNumOfPlayers, int MaxNumOfPlayers, int Duration) : this(Name, MinNumOfPlayers, MaxNumOfPlayers, Duration, Duration) { }
+
+        public enum GameType { Strategy, Tile, War, Dice, Card, Party, CoOp, Lying}
+    }
+}
---
+++ Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿namespace BoardGameChooser
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.numPlayers = new System.Windows.Forms.NumericUpDown();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.numMinMinutes = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numMaxMinutes = new System.Windows.Forms.NumericUpDown();
+            this.btnChoose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.numPlayers)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinMinutes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMaxMinutes)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // numPlayers
+            // 
+            this.numPlayers.Location = new System.Drawing.Point(12, 27);
+            this.numPlayers.Name = "numPlayers";
+            this.numPlayers.Size = new System.Drawing.Size(54, 20);
+            this.numPlayers.TabIndex = 0;
+            this.numPlayers.Value = new decimal(new int[] {
+            2,
+            0,
+            0,
+            0});
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 11);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Players:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(93, 11);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Minimum Minutes:";
+            // 
+            // numMinMinutes
+            // 
+            this.numMinMinutes.Location = new System.Drawing.Point(96, 27);
+            this.numMinMinutes.Name = "numMinMinutes";
+            this.numMinMinutes.Size = new System.Drawing.Size(54, 20);
+            this.numMinMinutes.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(221, 11);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(94, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Maximum Minutes:";
+            // 
+            // numMaxMinutes
+            // 
+            this.numMaxMinutes.Location = new System.Drawing.Point(224, 27);
+            this.numMaxMinutes.Maximum = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.numMaxMinutes.Name = "numMaxMinutes";
+            this.numMaxMinutes.Size = new System.Drawing.Size(54, 20);
+            this.numMaxMinutes.TabIndex = 4;
+            this.numMaxMinutes.Value = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            // 
+            // btnChoose
+            // 
+            this.btnChoose.Location = new System.Drawing.Point(353, 24);
+            this.btnChoose.Name = "btnChoose";
+            this.btnChoose.Size = new System.Drawing.Size(75, 23);
+            this.btnChoose.TabIndex = 6;
+            this.btnChoose.Text = "Choose";
+            this.btnChoose.UseVisualStyleBackColor = true;
+            this.btnChoose.Click += new System.EventHandler(this.btnChoose_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(514, 83);
+            this.Controls.Add(this.btnChoose);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.numMaxMinutes);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.numMinMinutes);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.numPlayers);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.numPlayers)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMinMinutes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numMaxMinutes)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.NumericUpDown numPlayers;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.NumericUpDown numMinMinutes;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numMaxMinutes;
+        private System.Windows.Forms.Button btnChoose;
+    }
+}
+
